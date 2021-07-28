ef4be037b53ab7ab0ea5fd3627e2449c33864fe4
DjCod3r <djonatanschvambach@gmail.com>
2021-07-28 13:54:37 -0300

    Path

---
+++ io/UsingBlock/ClassePath/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace ClassePath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Djona\OneDrive\Área de Trabalho\teste\File.txt";
+
+            Console.WriteLine(Path.GetFullPath(path));
+            Console.WriteLine(Path.GetFileName(path));
+            Console.WriteLine(Path.GetDirectoryName(path));
+        }
+    }
+}

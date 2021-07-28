171b30d6412cf38c207c2f5bbe33b2fced0d882b
DjCod3r <djonatanschvambach@gmail.com>
2021-07-28 12:39:29 -0300

    Directory

---
+++ io/UsingBlock/Diretorio/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace Diretorio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Djona\OneDrive\Área de Trabalho\teste";
+            try
+            {
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}

f14fe5d63c74c2ae8c6c3673a767c3e776cb1ef0
DjCod3r <djonatanschvambach@gmail.com>
2021-07-28 12:00:31 -0300

    Io

---
+++ io/UsingBlock/StreamWriterr/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace StreamWriterr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string spath = @"C:\Users\Djona\OneDrive\Área de Trabalho\File.txt";
+            string tpath = @"C:\Users\Djona\OneDrive\Área de Trabalho\File1.txt";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(spath);
+                using (StreamWriter sw = File.AppendText(tpath))
+                {
+                    foreach(string line in lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+                
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}

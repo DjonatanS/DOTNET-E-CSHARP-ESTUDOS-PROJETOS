5e1d516545f548ee46960e8d2154eef18c87ceca
DjCod3r <djonatanschvambach@gmail.com>
2021-07-29 23:18:24 -0300

    Icomparable

---
+++ Interface/CompararObjetos/CompararObjetos/Entities/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace CompararObjetos.Entities
+{
+    class Employee : IComparable
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(string csvEmployee)
+        {
+            string[] vect = csvEmployee.Split(','); //separar o argumento baseado na virgula
+            Name = vect[0]; //antes da virgula;
+            Salary = double.Parse(vect[1], CultureInfo.InvariantCulture); //dps da vurgula
+        }
+
+        public override string ToString()
+        {
+            return $"Name : {Name} Salary : {Salary}";
+        }
+
+        public int CompareTo(object obj)
+        {
+            if(!(obj is Employee))
+            {
+                throw new ArgumentException("Argument is not an Employee");
+            }
+            Employee other = obj as Employee;
+            return Salary.CompareTo(other.Salary);
+        }
+    }
+}
---
+++ Interface/CompararObjetos/CompararObjetos/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using CompararObjetos.Entities;
+
+namespace CompararObjetos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Djona\OneDrive\Área de Trabalho\teste\File.txt";
+
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<Employee> list = new List<Employee>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(new Employee(sr.ReadLine()));
+                    }
+                    list.Sort();
+                    foreach(Employee emp in list)
+                    {
+                        Console.WriteLine(emp);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}

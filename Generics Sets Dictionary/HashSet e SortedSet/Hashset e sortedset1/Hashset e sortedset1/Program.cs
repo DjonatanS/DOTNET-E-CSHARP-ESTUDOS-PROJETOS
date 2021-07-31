a6cfd608b15ff16edae664cb4de1b65cc12f7880
DjCod3r <djonatanschvambach@gmail.com>
2021-07-31 09:46:23 -0300

    HasSet , SortedSet

---
+++ Generics Sets Dictionary/HashSet e SortedSet/Hashset e sortedset1/Hashset e sortedset1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hashset_e_sortedset1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+
+            set.Add("Djonatan");
+            set.Add("Maria");
+            set.Add("Alex");
+
+            Console.WriteLine(set.Contains("Djonatan"));
+            Console.WriteLine(set.Count);
+
+            foreach(string i in set)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ Generics Sets Dictionary/HashSet e SortedSet/Hashset e sortedset1/SortedSet1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortedSet1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<string> set = new SortedSet<string>();
+            set.Add("Djonatna");
+            set.Add("Maria");
+            set.Add("Alex");
+            Console.WriteLine(set.Count);
+            Console.WriteLine(set.Contains("Maria"));
+            foreach(string i in set)
+            {
+                Console.WriteLine(i);
+            }
+
+            SortedSet<int> inteiros = new SortedSet<int>();
+            SortedSet<int> inteiros2 = new SortedSet<int>() { 1, 5, 6, 12 };
+            
+            inteiros.Add(10);
+            inteiros.Add(5);
+            inteiros.Add(3);
+            inteiros.Add(7);
+
+            PrintCollection(inteiros);
+
+            //union
+            SortedSet<int> unir = new SortedSet<int>(inteiros);
+            unir.UnionWith(inteiros2);
+            PrintCollection(unir);
+
+
+            //intersection
+            SortedSet<int> interseccao = new SortedSet<int>(inteiros);
+            interseccao.IntersectWith(inteiros2);
+            PrintCollection(interseccao);
+
+            //diference 
+            SortedSet<int> diference = new SortedSet<int>(inteiros);
+            diference.ExceptWith(inteiros2);
+            PrintCollection(diference);
+
+
+
+
+
+
+        }
+        static void PrintCollection<T>(IEnumerable<T> collection)
+        {
+            foreach(T obj in collection) {
+                Console.Write(obj + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}

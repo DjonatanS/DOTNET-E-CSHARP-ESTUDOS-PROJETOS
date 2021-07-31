e97ed380cc2cd5d97eb863e8fc50955f593ac80d
DjCod3r <djonatanschvambach@gmail.com>
2021-07-30 23:43:55 -0300

    GetHashCode

---
+++ Generics Sets Dictionary/GetHas/GetHash e Equals/GetHash e Equals/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using GetHash_e_Equals.Entities;
+namespace GetHash_e_Equals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "Maria";
+            string b = "Alex";
+            Console.WriteLine(a.Equals(b));
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+
+            Client c = new Client();
+            string name = "Djonatan";
+            c.Name = name;
+            c.Email = "Djo";
+
+            Client d = new Client();
+            d.Email = "Djo";
+
+            Console.WriteLine(c.GetHashCode());
+            Console.WriteLine(d.GetHashCode());
+        }
+    }
+}

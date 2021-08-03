17e767676059567211b964db7eecd909cf0880e0
DjCod3r <djonatanschvambach@gmail.com>
2021-08-02 22:25:59 -0300

    Equals

---
+++ Generics Sets Dictionary/HashSet e SortedSet/Comparação com HashSet e Equals/Comparação com HashSet e Equals/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Comparação_com_HashSet_e_Equals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            struct Point
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+            public Point(int x, int y) : this()
+            {
+                X = x;
+                Y = y;
+            }
+        }
+        
+        
+    }
+}
+    
+

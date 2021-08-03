6a7214bdaf73052afb8c5f7ccf52e66094b7e17d
DjCod3r <djonatanschvambach@gmail.com>
2021-08-03 13:16:41 -0300

    ConjuntoHash

---
+++ Generics Sets Dictionary/HashSet e SortedSet/Comparação com HashSet e Equals/ExConjuntos/Entities/LogRecord.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExConjuntos.Entities
+{
+    class LogRecord
+    {
+        public string Username { get; set; }
+        public DateTime Instant { get; set; }
+
+        public override int GetHashCode()
+        {
+            return Username.GetHashCode();
+        }
+
+        public LogRecord(string username, DateTime instant)
+        {
+            Username = username;
+            Instant = instant;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if(!(obj is LogRecord))
+            {
+                return false;
+            }
+            LogRecord other = obj as LogRecord;
+            return Username.Equals(other.Username);
+        }
+    }
+}

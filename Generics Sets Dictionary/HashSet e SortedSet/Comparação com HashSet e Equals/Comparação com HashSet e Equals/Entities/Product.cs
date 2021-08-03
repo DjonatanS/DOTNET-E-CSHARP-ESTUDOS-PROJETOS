dc284685393eab58f9e0171f2b007ba1c79e182b
DjCod3r <djonatanschvambach@gmail.com>
2021-08-02 23:14:01 -0300

    Override HashCode e Equals

---
+++ Generics Sets Dictionary/HashSet e SortedSet/Comparação com HashSet e Equals/Comparação com HashSet e Equals/Entities/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Comparação_com_HashSet_e_Equals.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode() + Price.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+           if(!(obj is Product)) {
+                return false;
+           }
+            Product other = obj as Product; //Downcasting
+            return Name.Equals(other.Name) && Price.Equals(other.Price);
+        }
+    }
+}
--- Generics Sets Dictionary/HashSet e SortedSet/Comparação com HashSet e Equals/Comparação com HashSet e Equals/Program.cs
+++ Generics Sets Dictionary/HashSet e SortedSet/Comparação com HashSet e Equals/Comparação com HashSet e Equals/Program.cs
@@ -1,25 +1,24 @@
 ﻿using System;
-
+using Comparação_com_HashSet_e_Equals.Entities;
+using System.Collections.Generic;
 namespace Comparação_com_HashSet_e_Equals
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
-            struct Point
-        {
-            public int X { get; set; }
-            public int Y { get; set; }
-            public Point(int x, int y) : this()
-            {
-                X = x;
-                Y = y;
-            }
+            HashSet<Product> a = new HashSet<Product>();
+            a.Add(new Product("TV", 700));
+            a.Add(new Product("Iphone", 2700));
+
+            HashSet<Point> b = new HashSet<Point>();
+            b.Add(new Point(10, 10));
+            b.Add(new Point(5, 10));
+
+            Product prod = new Product("TV", 700);
+            Console.WriteLine(a.Contains(prod));
+
+
         }
-        
-        
     }
 }
-    
-

f2871574ad1c81bd4dd181fb9a9b8b6819a48d7a
DjCod3r <djonatanschvambach@gmail.com>
2021-08-04 12:49:07 -0300

    Dictionary

---
+++ Generics Sets Dictionary/Dictionary/Dictionary/Dictionary/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> cookies = new Dictionary<string, string>();
+            cookies["User"] = "Maria";
+            cookies["Email"] = "Maria@gmail.com";
+            cookies["Phone"] = "99283255";
+            cookies["Phone"] = "91824724";
+
+
+            Console.WriteLine(cookies["Phone"]) ;
+            cookies.Remove("Phone");
+
+            if (cookies.ContainsKey("Phone"))
+            {
+                Console.WriteLine(cookies["Phone"]);
+            }
+            else
+            {
+                Console.WriteLine("Can't find email value");
+            }
+
+            Console.WriteLine(cookies.Count) ;
+
+
+            foreach(KeyValuePair<string,string> cookie in cookies)
+            {
+                Console.WriteLine(cookie.Key + " " + cookie.Value);
+            }
+        }
+    }
+}

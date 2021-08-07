15652815dfaee208227a055d9ee61e6fd90dfdd5
DjCod3r <djonatanschvambach@gmail.com>
2021-08-07 00:18:51 -0300

    Comparison

---
+++ Lambda,LINQ,Delegate/CompT/CompT/Entities/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace CompT.Entities
+{
+    class Product: IComparable<Product>
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public int CompareTo(Product other)
+        {
+            return Name.ToUpper().CompareTo(other.Name.ToUpper()); 
+        }
+
+        public override string ToString()
+        {
+            return $"Nome: {Name} Preço: {Price}";
+        }
+
+     
+    }
+}
--- Lambda,LINQ,Delegate/CompT/CompT/Program.cs
+++ Lambda,LINQ,Delegate/CompT/CompT/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using CompT.Entities;
 
 namespace CompT
 {
@@ -6,7 +9,17 @@ namespace CompT
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Product> list = new List<Product>();
+            list.Add(new Product("TV", 840));
+            list.Add(new Product("IPhone", 2500));
+            list.Add(new Product("Notebook", 3201));
+
+            list.Sort();
+            foreach(Product produto in list)
+            {
+                Console.WriteLine(produto);
+            }
+
         }
     }
 }

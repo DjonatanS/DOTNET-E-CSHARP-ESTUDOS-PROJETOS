f353875cc4827b9fc89d66f41f67d79545cb8a9d
DjCod3r <djonatanschvambach@gmail.com>
2021-08-08 00:11:21 -0300

    Delegate
    Criação de Delegate , utilização , e Multicast Delegate

---
+++ Lambda,LINQ,Delegate/Delegate/IntroDelegate/IntroDelegate/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace IntroDelegate
+{
+
+    delegate double BinaryNumericOperation(double n1, double n2);
+    delegate double SingleNumericOperation(double n1);
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 20;
+
+            BinaryNumericOperation somar = CalculationService.Sum;
+            SingleNumericOperation area = CalculationService.Square;
+
+            
+            
+            Console.WriteLine(somar(a, b));
+            Console.WriteLine(area(a));
+        }
+    }
+}

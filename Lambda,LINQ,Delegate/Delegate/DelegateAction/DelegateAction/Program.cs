d90de6016372cd3e27460d9ef77b05c986f136aa
DjCod3r <djonatanschvambach@gmail.com>
2021-08-09 19:53:27 -0300

    Delegate
    Testes com os Delegates Func e Action

--- Lambda,LINQ,Delegate/Delegate/DelegateAction/DelegateAction/Program.cs
+++ Lambda,LINQ,Delegate/Delegate/DelegateAction/DelegateAction/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DelegateAction
 {
@@ -6,7 +7,31 @@ namespace DelegateAction
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Product> list = new List<Product>();
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+
+            //observação:
+            //Delegate action precisa ter uma Função static void que tem uma Classe como argumento
+
+            Action<Product> act = p => p.Price += p.Price * 0.10; //Metodo Lambda Expression
+            Action<Product> ato = UpdatePrice; //Metodo Passando Function Static
+
+
+            list.ForEach(act);
+
+            foreach(Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+        }
+        
+        static void UpdatePrice(Product p)
+        {
+            p.Price += p.Price * 0.1;
         }
     }
 }
---
+++ Lambda,LINQ,Delegate/Delegate/FuncDelgate/FuncDelgate/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using FuncDelgate.Entities;
+using System.Collections.Generic;
+using System.Linq;
+namespace FuncDelgate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            Func<Product, string> func = NameUp;//Metodo padrão
+            Func<Product, string> funcEx = p => p.Name.ToUpper(); //Metodo Lambda
+
+            List<string> result = list.Select(func).ToList(); 
+
+
+
+            foreach(string s in result)
+            {
+                Console.WriteLine(s);
+            }
+        }
+
+        static string NameUp(Product p)
+        {
+            return p.Name.ToUpper();
+        }
+    }
+}
